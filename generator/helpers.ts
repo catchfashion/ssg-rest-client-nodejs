9966689ede13649007c891cd1bc2234a63511e8b
extjo <josungyun6518@gmail.com>
2022-03-16 17:41:05 +0900

    Add comment

--- generator/helpers.ts
+++ generator/helpers.ts
@@ -1,4 +1,5 @@
 export function toJSON<T>(text: string): T {
   // @note ssg api return non-json text.
+  // @see https://eapi.ssgadm.com/resources/js/info/ssgApi.js
   return eval(`(${text})`) as T;
 }
